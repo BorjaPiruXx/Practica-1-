5967251e4b02ffe3cf416f7b9077da08a682a388
BorjaPiruXx <bormaral13@gmail.com>
2019-04-13 13:10:04 +0200

    creacion de matriz resultado

--- Puzzle.py
+++ Puzzle.py
@@ -7,12 +7,21 @@ Created on Wed Apr 10 19:26:33 2019
 
 import numpy as np
 
-aleatorios = np.random.choice(np.arange(16),16,replace=False)
+lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0]
+
+def sonIguales(vector,lista):
+        for i in range(16):
+            if vector[i] != lista[i]:
+                return False
+        return True
 
 class Puzzle:
     
-    def __init__(self): 
-        self.matriz = np.array(aleatorios).reshape(4,4)
+    def __init__(self,vector):
+        if sonIguales(vector,lista):
+            self.matriz = np.array(lista).reshape(4,4)
+        else:
+            self.matriz = np.array(vector).reshape(4,4)
         self.filas = 4
         self.columnas = 4
     def toString(self):
--- main.py
+++ main.py
@@ -5,9 +5,19 @@ Created on Fri Apr 12 13:42:58 2019
 @author: ECI ALCORCON
 """
 
-from prueba1 import Puzzle
+from Puzzle import Puzzle
+import numpy as np
 
-""" Crear matriz de numeros aleatorios """
-matriz = Puzzle()
-""" Mostrar matriz por consola """
+aleatorios = np.random.choice(np.arange(16),16,replace=False)
+matriz = Puzzle(aleatorios)
+print("Matriz generada aleatoriamente")
 matriz.toString()
+print()
+
+print("Matriz resultado")
+resultado = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0]
+matrizResultado = Puzzle(resultado)
+matrizResultado.toString()
+print()
+
+
