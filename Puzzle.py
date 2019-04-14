f24a3f0948cbdcf04272bcfad803870d0709b091
BorjaPiruXx <bormaral13@gmail.com>
2019-04-14 22:41:59 +0200

    nuevos metodos
    mostrar posicion libre
    mostrar numeros adyacentes al hueco

--- Puzzle.py
+++ Puzzle.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Apr 10 19:26:33 2019
 
-@author: ECI ALCORCON
+@author: Yolanda Gomez Henche - Borja Martin Alonso
 """
 
 import numpy as np
@@ -15,6 +15,67 @@ def sonIguales(vector,lista):
                 return False
         return True
 
+"""def esSolucion(matriz,solucion):
+    for i in range(matriz):
+        for j in range(matriz[0]):
+            if matriz[i][j] != solucion[i][j]:
+                return False
+    return True"""
+
+def buscarHueco(matriz):
+    hueco = (0,0)
+    for i in range(4):
+        for j in range(4):
+            if matriz[i][j] == 0:
+                hueco = (i,j)
+    return hueco
+
+def numAdyacentes(adyacentes):
+    cont = 0
+    for i in range(adyacentes):
+        cont+=1
+    return cont
+
+def buscarAdyacentes(matriz,iHueco,jHueco):
+    adyacentes = []
+    if iHueco == 0:
+        if jHueco == 0:
+            adyacentes.append(matriz[iHueco][jHueco+1])
+            adyacentes.append(matriz[iHueco+1][jHueco])
+        elif jHueco > 0 and jHueco < 3:
+            adyacentes.append(matriz[iHueco][jHueco-1])
+            adyacentes.append(matriz[iHueco][jHueco+1])
+            adyacentes.append(matriz[iHueco+1][jHueco])
+        else:
+            adyacentes.append(matriz[iHueco][jHueco-1])
+            adyacentes.append(matriz[iHueco+1][jHueco])
+    elif iHueco > 0 and iHueco < 3:
+        if jHueco == 0:
+            adyacentes.append(matriz[iHueco-1][jHueco])
+            adyacentes.append(matriz[iHueco][jHueco+1])
+            adyacentes.append(matriz[iHueco+1][jHueco])
+        elif jHueco > 0 and jHueco < 3:
+            adyacentes.append(matriz[iHueco-1][jHueco])
+            adyacentes.append(matriz[iHueco][jHueco-1])
+            adyacentes.append(matriz[iHueco][jHueco+1])
+            adyacentes.append(matriz[iHueco+1][jHueco])
+        else:
+            adyacentes.append(matriz[iHueco-1][jHueco])
+            adyacentes.append(matriz[iHueco][jHueco-1])
+            adyacentes.append(matriz[iHueco+1][jHueco])
+    else:
+        if jHueco == 0:
+            adyacentes.append(matriz[iHueco-1][jHueco])
+            adyacentes.append(matriz[iHueco][jHueco+1])
+        elif jHueco > 0 and jHueco < 3:
+            adyacentes.append(matriz[iHueco-1][jHueco])
+            adyacentes.append(matriz[iHueco][jHueco-1])
+            adyacentes.append(matriz[iHueco][jHueco+1])
+        else:
+            adyacentes.append(matriz[iHueco-1][jHueco])
+            adyacentes.append(matriz[iHueco][jHueco-1])
+    return adyacentes
+
 class Puzzle:
     
     def __init__(self,vector):
@@ -24,8 +85,32 @@ class Puzzle:
             self.matriz = np.array(vector).reshape(4,4)
         self.filas = 4
         self.columnas = 4
+        self.movimientos = 0
+        self.hueco = buscarHueco(self.matriz)
+        self.adyacentes = buscarAdyacentes(self.matriz,self.hueco[0],self.hueco[1])
+        
     def toString(self):
         print(self.matriz)
+        
+    def getMovimientos(self):
+        return self.movimientos
+    
+    def mostrarHueco(self):
+            print(self.hueco[0],'',self.hueco[1])
+            
+    def mostrarAdyacentes(self):
+        tamAdyacentes = numAdyacentes(self.adyacentes)
+        for i in range(tamAdyacentes):
+            print(i,end='')
+        
+    """def recolocarPiezas(self,solucion):
+        while not esSolucion(self.matriz,solucion):
+            self.matriz.buscarHueco()
+            print('El hueco está situado en la posicion',hueco[0],'',hueco[1])
+            
+            
+        print('Se ha completado el puzzle')"""
+        
                 
 
     
--- main.py
+++ main.py
@@ -2,22 +2,32 @@
 """
 Created on Fri Apr 12 13:42:58 2019
 
-@author: ECI ALCORCON
+@author: Yolanda Gomez Henche - Borja Martin Alonso
 """
 
 from Puzzle import Puzzle
 import numpy as np
 
-aleatorios = np.random.choice(np.arange(16),16,replace=False)
-matriz = Puzzle(aleatorios)
-print("Matriz generada aleatoriamente")
-matriz.toString()
+print("Esta es la vista del puzzle resuelto")
 print()
-
-print("Matriz resultado")
 resultado = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0]
 matrizResultado = Puzzle(resultado)
 matrizResultado.toString()
 print()
+
+print('Usted debe generar ese mismo puzzle a partir de otro diferente',end='')
+print('cuyas posiciones estan colocadas aleatoriamente',end='.')
+print('¿Desea resolver el puzzle? [s/n]:')
+letra = input()
+if letra == 's' or letra == 'S':
+    aleatorios = np.random.choice(np.arange(16),16,replace=False)
+    matriz = Puzzle(aleatorios)
+    matriz.toString()
+    print()
+    print('Posicion libre:',matriz.mostrarHueco())
+    print('Numeros adyacentes al hueco:',matriz.mostrarAdyacentes())    
+else:
+    print('¡Hasta la proxima!')
+
 
 
